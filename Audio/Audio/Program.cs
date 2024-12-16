71e61af090e97e374e19507d5c19074d9707a4c5
Daniel Miziołek <143220728+Daniel-Miziolek@users.noreply.github.com>
2024-12-16 16:25:58 +0100

    Add files via upload

---
+++ Audio/Audio/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using NAudio.Wave;
+using System.Threading.Tasks;
+
+namespace Audio
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            while (true)
+            {
+                string path = @"C:\Users\Daniel\Desktop\Audio";
+                string[] folderWithAudios = Directory.GetFileSystemEntries(path);
+
+                for (int i = 0; i < folderWithAudios.Length; i++)
+                {
+                    Console.WriteLine($"Index: {i}     Name of the music: {Path.GetFileName(folderWithAudios[i])}");
+                }
+
+                Console.Write("Enter index of music, you want to play: ");
+                if (!int.TryParse(Console.ReadLine(), out int input) || input < 0 || input >= folderWithAudios.Length)
+                {
+                    Console.WriteLine("Invalid index. Press any key to try again.");
+                    Console.ReadKey();
+                    Console.Clear();
+                    continue;
+                }
+
+                string selectedMusic = Path.GetFileName(folderWithAudios[input]);
+                Console.WriteLine($"Selected music: {selectedMusic}");
+
+                using (var audioFile = new AudioFileReader(folderWithAudios[input]))
+                using (var outputDevice = new WaveOutEvent())
+                {
+                    Stopwatch stopwatch = new Stopwatch();
+                    outputDevice.Init(audioFile);
+                    outputDevice.Play();
+                    stopwatch.Start();
+                    TimeSpan totalTime = audioFile.TotalTime;
+
+                    string formattedTime = $"{totalTime.Hours:D2}:{totalTime.Minutes:D2}:{totalTime.Seconds:D2}";
+                    string formattedTimer = $"{stopwatch.Elapsed.Hours:D2}:{stopwatch.Elapsed.Minutes:D2}:{stopwatch.Elapsed.Seconds:D2}";
+
+                    bool isPlaying = true;
+
+                    _ = Task.Run(async () =>
+                    {
+                        while (isPlaying)
+                        {
+                            Console.Clear();
+                            Console.WriteLine($"Selected music: {selectedMusic}");
+                            Console.WriteLine($"Actual time of music: {stopwatch.Elapsed:hh\\:mm\\:ss}  ---   Total time: {formattedTime}");
+                            Console.WriteLine("[E] Pause | [Y] Stop");
+
+                            if (audioFile.TotalTime <= stopwatch.Elapsed)
+                            {
+                                Console.WriteLine("Music has ended");
+                                outputDevice.Stop();
+                                stopwatch.Stop();
+                                isPlaying = false;
+                                break;
+                            }
+
+                            await Task.Delay(1000);
+                        }
+                    });
+
+                    //else if (audioFile.TotalTime <= stopwatch.Elapsed)
+                    //{
+                    //    Console.WriteLine("Music has ended");
+                    //    outputDevice.Stop();
+                    //    stopwatch.Stop();
+                    //    isPlaying = false;
+                    //    break;
+                    //}
+
+                    while (true)
+                    {
+                        if (!isPlaying)
+                        {
+                            break;
+                        } 
+                        else
+                        {
+                            ConsoleKeyInfo key = Console.ReadKey(true);
+                            if (key.Key == ConsoleKey.E)
+                            {
+                                if (outputDevice.PlaybackState == PlaybackState.Playing)
+                                {
+                                    outputDevice.Pause();
+                                    stopwatch.Stop();
+                                }
+                                else
+                                {
+                                    outputDevice.Play();
+                                    stopwatch.Start();
+                                }
+                            }
+                            else if (key.Key == ConsoleKey.Y)
+                            {
+                                outputDevice.Stop();
+                                stopwatch.Stop();
+                                isPlaying = false;
+                                break;
+                            }
+                        }                                                
+                    }
+                }
+
+                Console.Clear();
+            }
+        }
+    }
+}
