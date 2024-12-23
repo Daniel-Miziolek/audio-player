ec3f22b1c504bff2a1da91e336dd466604dfa6fb
Daniel Miziołek <143220728+Daniel-Miziolek@users.noreply.github.com>
2024-12-24 00:47:25 +0100

    Add the ability to import music

--- Audio/Audio/Program.cs
+++ Audio/Audio/Program.cs
@@ -74,147 +74,68 @@ namespace Audio
                         continue;
                 }
             }
-        }        
+        }
 
         static void ImportMusic(List<string> folderWithAudios)
         {
-            string currentPath = @"C:\";
-            Stack<string> pathHistory = new Stack<string>();
-
-            string[] entries = Directory.GetFileSystemEntries(currentPath);
-            int currentLine = 0;
-            int topLine = 0;
-            int windowHeight = Console.WindowHeight;
-
-            Console.Clear();
-            DisplayEntries(entries, currentLine, topLine, windowHeight);
+            string currentPath = @"C:\Users\Daniel";            
 
             while (true)
             {
-                ConsoleKeyInfo keyInfo = Console.ReadKey(intercept: true);
-
-                if (keyInfo.Key == ConsoleKey.DownArrow)
+                try
                 {
-                    if (currentLine < entries.Length - 1)
+                    var entries = Directory.GetFileSystemEntries(currentPath)
+                        .Where(entry => Directory.Exists(entry) || IsAudioFile(entry)) 
+                        .ToList();
+         
+                    Stack<string> pathHistory = new Stack<string>();
+
+                    var selectedMusicOrFolder = AnsiConsole.Prompt(
+                        new SelectionPrompt<string>()
+                            .Title("Choose music to import")
+                            .PageSize(10)
+                            .MoreChoicesText("[grey](Move up and down to reveal more folders and files)[/]")
+                            .AddChoices(entries));                            
+
+                    if (Directory.Exists(selectedMusicOrFolder))
                     {
-                        currentLine++;
-                        if (currentLine >= topLine + windowHeight)
+                        currentPath = selectedMusicOrFolder;
+                    }
+                    else if (File.Exists(selectedMusicOrFolder))
+                    {
+                        if (folderWithAudios.Contains(selectedMusicOrFolder))
                         {
-                            topLine++;
+                            AnsiConsole.MarkupLine($"[red]This music has already been imported: {Path.GetFileName(selectedMusicOrFolder)}[/]");
+                            Console.WriteLine("Press any key to continue...");
+                            Console.ReadKey();
                         }
-                        DisplayEntries(entries, currentLine, topLine, windowHeight);
-                    }
-                }
-                else if (keyInfo.Key == ConsoleKey.UpArrow)
-                {
-                    if (currentLine > 0)
-                    {
-                        currentLine--;
-                        if (currentLine < topLine)
+                        else
                         {
-                            topLine--;
-                        }
-                        DisplayEntries(entries, currentLine, topLine, windowHeight);
+                            folderWithAudios.Add(selectedMusicOrFolder);
+                            AnsiConsole.MarkupLine($"[green]Add: {Path.GetFileName(selectedMusicOrFolder)}[/]");
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+                        }                        
                     }
                 }
-                else if (keyInfo.Key == ConsoleKey.Enter)
+                catch (Exception ex)
                 {
-                    string path = entries[currentLine];
-                    if (Directory.Exists(path))
-                    {
-                        pathHistory.Push(currentPath);
-                        currentPath = path;
-                        entries = Directory.GetFileSystemEntries(currentPath);
-                        Console.Clear();
-                        currentLine = 0;
-                        topLine = 0;
-                        DisplayEntries(entries, currentLine, topLine, windowHeight);
-                    }
-                    else if (File.Exists(path))
-                    {
-                        OpenFile(path, folderWithAudios);
-                    }
-                }
-                else if (keyInfo.Key == ConsoleKey.LeftArrow)
-                {
-                    if (pathHistory.Count > 0)
-                    {
-                        currentPath = pathHistory.Pop();
-                        entries = Directory.GetFileSystemEntries(currentPath);
-                        Console.Clear();
-                        currentLine = 0;
-                        topLine = 0;
-                        DisplayEntries(entries, currentLine, topLine, windowHeight);
-                    }
-                }
-                else if (keyInfo.Key == ConsoleKey.Escape)
-                {
-                    Console.Clear();
-                    break;
+                    AnsiConsole.MarkupLine($"[red]Error: {ex.Message}[/]");
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
                 }
             }
+
         }
 
-        static void DisplayEntries(string[] entries, int currentLine, int topLine, int windowHeight)
+        static bool IsAudioFile(string path)
         {
-            Console.Clear();
-
-            for (int i = topLine; i < Math.Min(topLine + windowHeight, entries.Length); i++)
-            {
-                bool accessDenied = false;
-
-                if (Directory.Exists(entries[i]))
-                {
-                    try
-                    {
-                        Directory.GetFileSystemEntries(entries[i]);
-                    }
-                    catch (UnauthorizedAccessException)
-                    {
-                        accessDenied = true;
-                    }
-                }
-
-                if (i == currentLine)
-                {
-                    HighlightLine(i - topLine, entries[i], accessDenied);
-                }
-                else
-                {
-                    if (accessDenied)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                    }
-                    Console.SetCursorPosition(0, i - topLine);
-                    Console.WriteLine(entries[i]);
-                    Console.ResetColor();
-                }
-            }
+            string[] allowedExtensions = { ".mp3", ".wav", ".flac", ".ogg" };
+            return allowedExtensions.Contains(Path.GetExtension(path), StringComparer.OrdinalIgnoreCase);
         }
 
-        static void HighlightLine(int lineIndex, string entry, bool accessDenied)
-        {
-            Console.SetCursorPosition(0, lineIndex);
-            Console.BackgroundColor = ConsoleColor.Blue;
-            Console.ForegroundColor = accessDenied ? ConsoleColor.Red : ConsoleColor.White;
-            Console.WriteLine(entry);
-            Console.ResetColor();
-        }
-
-        static void OpenFile(string path, List<string> folderWithAudios)
-        {
-            if (!folderWithAudios.Contains(path))
-            {
-                folderWithAudios.Add(path);
-                Console.WriteLine($"File {Path.GetFileName(path)} added to the list.");
-            }
-            else
-            {
-                Console.WriteLine($"File {Path.GetFileName(path)} is already in the list.");
-            }
-        }
-
-        private static void PlayMusic(List<string> folderWithAudios, int input)
+        static void PlayMusic(List<string> folderWithAudios, int input)
         {
 
             string selectedMusic = Path.GetFileName(folderWithAudios[input]);
@@ -255,15 +176,6 @@ namespace Audio
                         await Task.Delay(1000);
                     }
                 });
-
-                //else if (audioFile.TotalTime <= stopwatch.Elapsed)
-                //{
-                //    Console.WriteLine("Music has ended");
-                //    outputDevice.Stop();
-                //    stopwatch.Stop();
-                //    isPlaying = false;
-                //    break;
-                //}
 
                 while (true)
                 {
